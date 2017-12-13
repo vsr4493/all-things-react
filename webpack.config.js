07f84315d4695cdce9ac4e930b94816e50f76361
Vardhman Singh <vardhman.singh@1mg.com>
2017-12-13 23:18:11 +0530

    chore: add babel and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
-var path = require('path');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var PATHS = {
 	JS: path.resolve(__dirname, "src"),
-	DIST: path.resolve(__dirname, "dist")
+	DIST: path.resolve(__dirname, "dist"),
+	PUBLIC: path.resolve(__dirname, "public")
 };
 
 var config = {
-	entry: [
-		path.join(PATHS.JS, "app.js")
-	],
+	entry: [path.join(PATHS.JS, "app.js")],
 	output: {
 		path: PATHS.DIST,
-		filename: 'bundle.js'
+		filename: "bundle.js"
+	},
+	devServer: {
+		contentBase: PATHS.PUBLIC
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(PATHS.PUBLIC, "index.html")
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					"babel-loader",
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx"]
 	}
-}
+};
 
 module.exports = config;
